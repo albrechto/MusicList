52f2d9fd7bd7aa5da03c3259f4121598213eb97b
FrancisB1 <gaspump1112@gmail.com>
2015-03-09 10:05:25 -0400

    music changes

--- Song.java
+++ Song.java
@@ -2,13 +2,21 @@ public class Song {
   
   public String artist;
   public String name;
+  public String year;
+  public String score;
+  public String notes;
   // More fields here
   
   // Default constructor.. you'll need to modify this
   public Song(String artist, String name) { 
     this.artist = artist;
     this.name = name;
+    this.year = year;
+    this.score = score;
+    this.notes = notes;
+  
   }
+  
   
   /* ADD YOUR CODE HERE */
   
